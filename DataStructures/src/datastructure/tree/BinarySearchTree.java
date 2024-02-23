5247b4f9ed01dac81a4d8e9babee75aff9bb7216
Gokul-nath-K <gokulnathk003@gmail.com>
2024-02-23 16:22:21 +0530

    Node is separated

--- DataStructures/src/datastructure/tree/BinarySearchTree.java
+++ DataStructures/src/datastructure/tree/BinarySearchTree.java
@@ -2,17 +2,6 @@ package datastructure.tree;
 
 public class BinarySearchTree {
 
-    static class Node {
-
-        private int data;
-        private Node right;
-        private Node left;
-
-        public Node(int data) {
-            this.data = data;
-        }
-    }
-
     private Node top;
     Node insertElement(Node current, int data) {
 
